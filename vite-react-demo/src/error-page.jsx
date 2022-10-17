6e4692d8f79595af0cf96bc25d1a42ed8c5add37
duxiaolei <duxiaolei@mabangerp.com>
2022-10-17 18:56:31 +0800

    test

--- vite-react-demo/src/error-page.jsx
+++ vite-react-demo/src/error-page.jsx
@@ -2,7 +2,7 @@ import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
   const error = useRouteError()
-
+  console.log(error)
   return (
     <div id="error-page">
       <h1>page 404 啦！</h1>
